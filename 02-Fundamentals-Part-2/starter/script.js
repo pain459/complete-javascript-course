0992371b98fd15bb6c8e1fb81ecd3da55d63a6a5
pain459 <ravi.ameer@gmail.com>
2021-11-29 13:13:13 +0530

    upto dot and braket notations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -302,6 +302,7 @@ console.log(friends.indexOf('Bob'));
 console.log(friends.includes('Steven'));
 */
 
+/*
 // Coding challenge 2
 const calcTip = totalBill => {
     if (totalBill >= 50 && totalBill <= 300) {
@@ -322,3 +323,94 @@ total.push(bills[0] + tips[0]);
 total.push(bills[1] + tips[1]);
 total.push(bills[2] + tips[2]);
 console.log(total);
+*/
+
+/*
+// Introduction to Objects
+
+// traditional array without reference.
+const jonasArray = [
+    'jonas',
+    'jonas1',
+    2037 - 1991,
+    'teacher',
+    ['Mike', 'Peter', 'Steven']
+];
+
+// Objects creates k:v pairs like in Python.
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Jonas1',
+    age: 2037 - 1991,
+    job: 'Teacher',
+    friends: ['Mike', 'Peter', 'Steven']
+}
+
+console.log(jonas['firstName']);
+
+*/
+
+/*
+// Dot vs Bracker Notation
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Jonas1',
+    age: 2037 - 1991,
+    job: 'Teacher',
+    friends: ['Mike', 'Peter', 'Steven']
+}
+
+console.log(jonas);
+
+console.log(jonas.lastName);  // dot notation
+// . is an operator
+console.log(jonas['firstName']);
+const nameKey = 'Name';
+console.log(jonas[nameKey]);
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, job, friends');
+// console.log(jonas.interestedIn);  // this doesn't work
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, job, friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonass';
+console.log(jonas);
+
+// challenge
+// Jonas has 3 friends and his best friend is called Michael.
+console.log(`Jonas has ${jonas.friends.length} friends and his best friend is ${jonas.friends[0]}`);
+*/
+/*
+// assignment: introduction to objects
+
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'multiple',
+    population: 130,
+    neighbours: ['China', 'Pakistan', 'Nepal', 'Bhutan', 'SriLanka']
+}
+
+// const queryCountryDetails = prompt('details for country India. query now.');
+
+// if (myCountry[queryCountryDetails]) {
+//     console.log(myCountry[queryCountryDetails]);
+// } else {
+//     console.log('Incorrect details queried!');
+// }
+
+console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language} language speaking people, ${myCountry.neighbours.length} neighbouring countries and capital called ${myCountry.capital}`);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry['population'] -= 2;
+console.log(myCountry.population);
+*/
